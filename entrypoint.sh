9c15b76e9ca400b8896a6d79bb01fdbdabd840e1
x80486 <masked.github@mailgw.com>
2021-07-12 11:32:13 -0400

    smartsynchronize release 4.2.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,5 @@ set -o errexit
 
 SMARTSYNCHRONIZE_JAVA_HOME="/app/extra/smartsynchronize/jre"
 export SMARTSYNCHRONIZE_JAVA_HOME
-TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
-export TMPDIR
 
 exec /app/extra/smartsynchronize/bin/smartsynchronize.sh "$@"
